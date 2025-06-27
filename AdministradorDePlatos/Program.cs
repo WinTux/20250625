c18b13f07bd169f77d5e4fdca6bfcfe1430c3a18
WinTux <rusok.software@gmail.com>
2025-06-27 15:27:42 -0400

    Completando los endpoints

--- AdministradorDePlatos/Program.cs
+++ AdministradorDePlatos/Program.cs
@@ -1,4 +1,5 @@
 using AdministradorDePlatos.Conexion;
+using AdministradorDePlatos.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,5 +9,29 @@ var app = builder.Build();
 app.MapGet("api/plato", async (PlatoDbContext contexto) => { 
     var elementos = await contexto.Platos.ToListAsync();
     return Results.Ok(elementos);
+});
+
+app.MapPost("api/plato", async (PlatoDbContext contexto, Plato plato) => {
+    await contexto.Platos.AddAsync(plato);
+    await contexto.SaveChangesAsync();
+    return Results.Created($"api/plato/{plato.Id}",plato);
+});
+
+app.MapPut("api/plato/{id}", async (PlatoDbContext contexto, int id, Plato plato) => {
+    var platoEnDDBB = await contexto.Platos.FirstOrDefaultAsync(p => p.Id == id);
+    if(platoEnDDBB == null)
+        return Results.NotFound();
+    platoEnDDBB.Nombre = plato.Nombre;
+    await contexto.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+app.MapDelete("api/plato/{id}", async (PlatoDbContext contexto, int id) => {
+    var platoEnDDBB = await contexto.Platos.FirstOrDefaultAsync(p => p.Id == id);
+    if (platoEnDDBB == null)
+        return Results.NotFound();
+    contexto.Platos.Remove(platoEnDDBB);
+    await contexto.SaveChangesAsync();
+    return Results.NoContent();
 });
 app.Run();
